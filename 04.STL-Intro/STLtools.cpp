95702d2be73f9bee7ac2fd3742901e3c2092e772
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-08-24 21:52:18 +0530

    commit on 24/08/2020

---
+++ 04.STL-Intro/STLtools.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+                     //VECTORS
+//    vector<int> v;
+//    for(int i = 0;i<5;i++)
+//    {
+//        v.push_back(i+1);
+//    }
+//    cout<<endl;
+//    vector<int>::iterator it;
+//    for(it=v.begin();it!=v.end();it++)
+//    {
+//        cout<<*it<<" ";
+//    }
+
+
+
+                  //STRINGS
+    //String Slicing
+//    string s1 = "Shreya";
+//    string s2(s1,2,3);
+//    cout<<s1<<" "<<s2;
+    //String concatention
+//    string s1 = "Shreya";
+//    string s2 = "Sharma";
+//
+//    cout<<s1+" "+s2;
+ //Sub-strings
+//    string s1 = "Shreya Sharma";
+//    string s2 = s1.substr(2,6);
+//    string s3(s1,2,6);
+//    cout<<s2<<" "<<s3;
+
+//
+//string s1 = "Shreya";
+//cout<<s1.substr(1);
+//
+
+        //PAIR CLASS
+
+//    pair<int,char> p1(36,'S');
+//    cout<<p1.first<<" "<<p1.second<<endl;
+//
+//    pair<int,int> p2(1,2);
+//    pair<int,pair<int,int>>p3(9,p2);
+//    cout<<p3.first<<" "<<p3.second.first<<" "<<p3.second.second<<endl;
+
+
+        //SET - to store unique values  The Working is done using Binary search Tree = O(nlog n)
+//
+//    set<int> s;
+//    int arr[] = {1,2,3,4,5,6,5,9};
+//
+//    for(int i =0;i<8;i++)
+//    {
+//        s.insert(arr[i]);
+//    }
+//    set<int>::iterator it;
+//    for(it = s.begin();it!=s.end();it++)
+//    {
+//        cout<<*it<<" ";
+//    }
+//    cout<<endl;
+//    if(s.find(7) == s.end())   //TO FIND IF ELEMNT PRESENT
+//        cout<<"Not Found!";
+//    else
+//        cout<<"Found!";
+//
+//    return 0;
+}
